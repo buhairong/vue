f3284cfe41d78a08311fd7ed20ddf4a6667aa847
buhairong <22858111@qq.com>
2018-10-09 06:58:56 +0800

    addfile

--- code/VuexTest/src/App.vue
+++ code/VuexTest/src/App.vue
@@ -1,19 +1,47 @@
 <template>
-  <div class="container">
-    <Search/>
-    <UsersMain/>
+  <div>
+    <p>click {{count}} times,count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
   </div>
 </template>
 
 <script>
-  import Search from './components/Search.vue'
-  import Main from './components/Main.vue'
-
   export default {
-    components: {
-      Search,
-      UsersMain: Main
-    }
+      data () {
+          return {
+              count:0
+          }
+      },
+      computed: {
+          evenOrOdd () {
+            return this.count%2===0?"偶数":"奇数"
+          }
+      },
+      methods: {
+          increment () {
+              const count = this.count;
+              this.count = count + 1;
+          },
+          decrement () {
+              const count = this.count;
+              this.count = count - 1;
+          },
+          incrementIfOdd () {
+              const count = this.count;
+              if(count%2===1){
+                  this.count = count + 1;
+              }
+          },
+          incrementAsync () {
+              setTimeout(()=>{
+                  const count = this.count;
+                  this.count = count + 1;
+              },1000)
+          }
+      }
   }
 </script>
 
--- code/VuexTest/src/main.js
+++ code/VuexTest/src/main.js
@@ -3,21 +3,10 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
-import store from './vuex/store'
 
 
-// 创建vm
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  store
+    el: '#app',
+    components:{App},
+    template:'<App/>'
 })
-
-/*
-new Vue({
-  el: '#app',
-  components: {App}, // 映射组件标签
-  template: '<App/>', // 指定需要渲染到页面的模板
-  store
-})*/
